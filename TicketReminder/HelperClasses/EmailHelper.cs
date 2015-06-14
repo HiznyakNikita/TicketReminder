31968ef4e6cd6aa5fc3b23c52af58c0eacb1fbdc
JaspeTMFB <nikita.ru96@mail.ru>
2015-06-14 10:56:40 +0300

    Add helper classes (EmailSendHelper, VkHelper)

---
+++ TicketReminder/HelperClasses/EmailHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketReminder
+{
+    public class EmailHelper
+    {
+        public static void SendMail(string password,
+            string mailto, string caption, string message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(Properties.Settings.Default.UserDprcGovUaEmail);
+                mail.To.Add(new MailAddress(mailto));
+                mail.Subject = caption;
+                mail.Body = message;
+                SmtpClient client = new SmtpClient();
+                client.Host = Properties.Settings.Default.AppSmtpServer;
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(Properties.Settings.Default.UserDprcGovUaEmail.Split('@')[0], password);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mail);
+                mail.Dispose();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Mail.Send: " + e.Message);
+            }
+        }
+    }
+}
---
+++ TicketReminder/HelperClasses/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketReminder
+{
+    public class Helper
+    {
+        public static string ConvertDate(string dateToConvert)
+        {
+            string year = dateToConvert.Substring(6, 4);
+            string month = dateToConvert.Substring(3, 2);
+            string day = dateToConvert.Substring(0, 2);
+            return year + "-" + month + "-" + day;
+        }
+
+        public static CarType ConvertToCarType(string type)
+        {
+            switch(type)
+            {
+                case "Люкс":
+                    return CarType.Luxe;
+                case "Купэ фирменное":
+                    return CarType.CoupeFirm;
+                case "Купэ":
+                    return CarType.Coupe;
+                case "Плацкарт фирменный":
+                    return CarType.ReservedSeatFirm;
+                case "Плацкарт":
+                    return CarType.ReservedSeat;
+                case "Сидя":
+                    return CarType.Seat;
+                default:
+                    return CarType.Seat;
+            }
+        }
+    }
+}
